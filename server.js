1359fc1c1a71650a9f3d5e3a5c605518c713b833
Tadeu Oliveira <tadeu_oliveira@id.uff.br>
2021-08-18 18:50:47 -0300

    serving request information

--- server.js
+++ server.js
@@ -21,8 +21,15 @@ app.get("/", function (req, res) {
 
 
 // your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+app.get("/api/whoami", function (req, res) {
+
+  console.log({headers: req.get('accept-language')})
+
+  res.json({
+    ipaddress: req.host,
+    language: req.get('accept-language'),
+    software: req.get('user-agent'),
+  });
 });
 
 
